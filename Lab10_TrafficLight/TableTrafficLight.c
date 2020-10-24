ee3c36d5938f4502c3b772fe60de0fa94eb749ba
Sebastian Popa <73309171+Popa-Sebastian@users.noreply.github.com>
2020-10-24 20:22:25 +0300

    Add files via upload

--- Lab10_TrafficLight/TableTrafficLight.c
+++ Lab10_TrafficLight/TableTrafficLight.c
@@ -60,7 +60,7 @@ unsigned long Input;
 // Linked data structure
 struct State {
   unsigned long PBOut;
-	unsigned long PFOut;	
+  unsigned long PFOut;	
   unsigned long Time;  
   unsigned long Next[9];
 };
@@ -69,13 +69,16 @@ typedef const struct State STyp;
 #define GoW   		0
 #define WaitW 		1
 #define GoS   		2
-#define WaitS			3
-#define Walk			4
+#define WaitS		3
+#define Walk		4
 #define Flash1ON	5
 #define Flash1OFF	6
 #define Flash2ON	7
 #define Flash2OFF	8
-	
+
+// The functioning of the program is described with a Moore Finite State Machine defined in a Struct State
+// There are 9 total states. Every state has an output that only depends on the current state PBout and PFout
+// For every possible input (8 inputs from 000 to 111) there is a State Transition
 STyp FSM[9]={
  {0x0C, 0x02, 100,{GoW, GoW, WaitW, WaitW, WaitW, WaitW, WaitW, WaitW}},//0
  {0x14, 0x02, 50, {GoS, GoS, GoS, GoS, Walk, Walk, GoS, GoS}},//1
@@ -92,10 +95,12 @@ STyp FSM[9]={
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
+// Port Initializations
 void PortB_Init(void);
 void PortE_Init(void);
 void PortF_Init(void);
 
+// SysTick Initalization and Delay Functions
 void SysTick_Init(void);
 void SysTick_Wait10ms(unsigned long delay);
 void SysTick_Wait(unsigned long delay);
@@ -106,36 +111,36 @@ int main(void){
   TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210,ScopeOff); // activate grader and set system clock to 80 MHz
   EnableInterrupts();
 	
-	PortB_Init();			// OUTPUTS WEST:PB5-PB3=RYG, SOUTH:PB2-PB0=RYG
-	PortE_Init();			// INPUTS: PE2=WALK, PE1=SOUTH, PE0=WEST
-	PortF_Init();			// OUTPUTS PEDESTRIAN: PF3=G, PF1=R
-	SysTick_Init();		// Initializez SysTick for the Delay functions
+  PortB_Init();		// OUTPUTS WEST:PB5-PB3=RYG, SOUTH:PB2-PB0=RYG
+  PortE_Init();		// INPUTS: PE2=WALK, PE1=SOUTH, PE0=WEST
+  PortF_Init();		// OUTPUTS PEDESTRIAN: PF3=G, PF1=R
+  SysTick_Init();	// Initializez SysTick for the Delay functions
 	
-	State = GoW; 
-  while(1){
-		GPIO_PORTB_DATA_R = FSM[State].PBOut;
-		GPIO_PORTF_DATA_R = FSM[State].PFOut;
-		SysTick_Wait10ms(FSM[State].Time);
-		Input = GPIO_PORTE_DATA_R &0x07;
-		State = FSM[State].Next[Input];  
+  State = GoW; 
+  while(1) {
+	GPIO_PORTB_DATA_R = FSM[State].PBOut;
+	GPIO_PORTF_DATA_R = FSM[State].PFOut;
+	SysTick_Wait10ms(FSM[State].Time);
+	Input = GPIO_PORTE_DATA_R &0x07;
+	State = FSM[State].Next[Input];  
   }
 }
 
 // Intilizes PORT_B
 void PortB_Init(void) {
-	volatile unsigned long delay;
-	SYSCTL_RCGC2_R |= 0x02;  		 // 1) Port B Clock
-  delay = SYSCTL_RCGC2_R;      // 2) no need to unlock
-	GPIO_PORTB_AMSEL_R &= ~0x3F; // 3) disable analog function on PB5-0
+  volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x02; 	// 1) Port B Clock
+  delay = SYSCTL_RCGC2_R;	// 2) no need to unlock
+  GPIO_PORTB_AMSEL_R &= ~0x3F;	// 3) disable analog function on PB5-0
   GPIO_PORTB_PCTL_R &= ~0x00FFFFFF; // 4) enable regular GPIO
-  GPIO_PORTB_DIR_R |= 0x3F;    // 5) outputs on PB5-0
-  GPIO_PORTB_AFSEL_R &= ~0x3F; // 6) regular function on PB5-0
-  GPIO_PORTB_DEN_R |= 0x3F;    // 7) enable digital on PB5-0
+  GPIO_PORTB_DIR_R |= 0x3F;  	// 5) outputs on PB5-0
+  GPIO_PORTB_AFSEL_R &= ~0x3F;	// 6) regular function on PB5-0
+  GPIO_PORTB_DEN_R |= 0x3F;   	// 7) enable digital on PB5-0
 }
 // Intilizes PORT_E
 void PortE_Init(void) {
-	volatile unsigned long delay;
-	SYSCTL_RCGC2_R |= 0x10;      	// 1) PORT E Clock
+  volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x10;      	// 1) PORT E Clock
   delay = SYSCTL_RCGC2_R;      	// 2) no need to unlock
   GPIO_PORTE_AMSEL_R &= 0x00; 	// 3) disable analog function
   GPIO_PORTE_PCTL_R &= ~0x000000FF; // 4) enable regular GPIO
@@ -145,21 +150,21 @@ void PortE_Init(void) {
 }
 // Intilizes PORT_F
 void PortF_Init(void) {
-	volatile unsigned long delay;
-	SYSCTL_RCGC2_R |= 0x20;					// 1) Port F Clock
-	delay = SYSCTL_RCGC2_R;					// allow time for clock to start
-	GPIO_PORTF_LOCK_R = 0x4C4F434B;	// 2) unlock GPIO Port F
-	GPIO_PORTF_CR_R = 0x1F;					// allow changes to PF4-0
-	// only PF0 needs to be unlocked, other bits can't be locked
-	GPIO_PORTF_AMSEL_R = 0x00;    	// 3) disable analog on PF
+  volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x20;					// 1) Port F Clock
+  delay = SYSCTL_RCGC2_R;					// allow time for clock to start
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;	// 2) unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;					// allow changes to PF4-0
+  // only PF0 needs to be unlocked, other bits can't be locked	
+  GPIO_PORTF_AMSEL_R = 0x00;    	// 3) disable analog on PF
   GPIO_PORTF_PCTL_R = 0x00000000; // 4) PCTL GPIO on PF4-0
-	GPIO_PORTF_DIR_R |= 0x0A;     	// 5) PF3, PF1 OUT
+  GPIO_PORTF_DIR_R |= 0x0A;     	// 5) PF3, PF1 OUT
   GPIO_PORTF_AFSEL_R = 0x00;    	// 6) disable alt funct on PF7-0
   //GPIO_PORTF_PUR_R = 0x11;    	// enable pull-up on PF0 and PF4
   GPIO_PORTF_DEN_R = 0x1F;      	// 7) enable digital I/O on PF4-0
 }
 
-// Initializez SysTick (precision clock and external crystal)
+// Initializez SysTick (clock counter, very precise when using external crystal and PLL)
 void SysTick_Init(void){
   NVIC_ST_CTRL_R = 0;             // disable SysTick during setup
   NVIC_ST_CTRL_R = 0x00000005;    // enable SysTick with core clock
