7f83e0dbacc343c8939a2a002c5ed622f51fb32a
Sebastian Popa <73309171+Popa-Sebastian@users.noreply.github.com>
2020-10-24 21:43:13 +0300

    Update TableTrafficLight.c

--- Lab10_TrafficLight/TableTrafficLight.c
+++ Lab10_TrafficLight/TableTrafficLight.c
@@ -77,7 +77,7 @@ typedef const struct State STyp;
 #define Flash2OFF	8
 
 // The functioning of the program is described with a Moore Finite State Machine defined in a Struct State
-// There are 9 total states. Every state has an output that only depends on the current state PBout and PFout
+// There are 9 total states. Every state has an output (PBout and PFout) that only depends on the current state 
 // For every possible input (8 inputs from 000 to 111) there is a State Transition
 STyp FSM[9]={
  {0x0C, 0x02, 100,{GoW, GoW, WaitW, WaitW, WaitW, WaitW, WaitW, WaitW}},//0
