c9244c9fd921e776d3a589256729a61367c6e201
Sebastian Popa <73309171+Popa-Sebastian@users.noreply.github.com>
2020-10-24 21:30:33 +0300

    Update TableTrafficLight.c

--- Lab10_TrafficLight/TableTrafficLight.c
+++ Lab10_TrafficLight/TableTrafficLight.c
@@ -150,17 +150,17 @@ void PortE_Init(void) {
 // Intilizes PORT_F
 void PortF_Init(void) {
   volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x20;					// 1) Port F Clock
-  delay = SYSCTL_RCGC2_R;					// allow time for clock to start
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;	// 2) unlock GPIO Port F
-  GPIO_PORTF_CR_R = 0x1F;					// allow changes to PF4-0
+  SYSCTL_RCGC2_R |= 0x20;	// 1) Port F Clock
+  delay = SYSCTL_RCGC2_R;	// allow time for clock to start
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;// 2) unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;	// allow changes to PF4-0
   // only PF0 needs to be unlocked, other bits can't be locked	
-  GPIO_PORTF_AMSEL_R = 0x00;    	// 3) disable analog on PF
+  GPIO_PORTF_AMSEL_R = 0x00;    // 3) disable analog on PF
   GPIO_PORTF_PCTL_R = 0x00000000; // 4) PCTL GPIO on PF4-0
-  GPIO_PORTF_DIR_R |= 0x0A;     	// 5) PF3, PF1 OUT
-  GPIO_PORTF_AFSEL_R = 0x00;    	// 6) disable alt funct on PF7-0
-  //GPIO_PORTF_PUR_R = 0x11;    	// enable pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R = 0x1F;      	// 7) enable digital I/O on PF4-0
+  GPIO_PORTF_DIR_R |= 0x0A;     // 5) PF3, PF1 OUT
+  GPIO_PORTF_AFSEL_R = 0x00;    // 6) disable alt funct on PF7-0
+  //GPIO_PORTF_PUR_R = 0x11;    // enable pull-up on PF0 and PF4
+  GPIO_PORTF_DEN_R = 0x1F;      // 7) enable digital I/O on PF4-0
 }
 
 // Initializez SysTick (clock counter, very precise when using external crystal and PLL)
